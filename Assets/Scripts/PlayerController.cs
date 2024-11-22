6bd69f365d2e439a25e94ac7072e286001bc05a4
Adam Anderson <anderada@sheridancollege.ca>
2024-11-22 02:38:09 +0000

    Task 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 0f;
+    public float acceleration = 0.1f;
+    public float maxSpeed = 5f;
+    public Vector2 playerInput;
+
     public enum FacingDirection
     {
         left, right
@@ -20,13 +26,36 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        float xInput = 0;
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            xInput = -1;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            xInput = 1;
+        }
+        playerInput = new Vector2(xInput, Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate(){
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if(playerInput.x > 0){
+            if(speed < 0) speed = 0;
+            speed = Mathf.Min(speed + acceleration, maxSpeed);
+        }
+        else if(playerInput.x < 0){
+            if(speed > 0) speed = 0;
+            speed = Mathf.Max(speed - acceleration, -maxSpeed);
+        }
+        else{
+            speed = 0;
+        }
 
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
     }
 
     public bool IsWalking()
